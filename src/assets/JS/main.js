23775d38d95c2e71e4c9ee393337c1c9beaa28cf
Keita-0025 <ku74n1@gmail.com>
2025-02-19 10:03:11 +0900

    feat:FirestoreのDBと接続しデータをfetchする機能を追加(READ)

--- src/assets/JS/main.js
+++ src/assets/JS/main.js
@@ -1,21 +1,49 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "daily-report-xxxx.firebaseapp.com",
-  projectId: "daily-report-xxxxx",
-  storageBucket: "daily-report-xxxxxx.firebasestorage.app",
-  messagingSenderId: "xxxxxxxxxxx",
-  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  measurementId: "xxxxxxxxx"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+console.log(app)
+// const analytics = getAnalytics(app);
+
+const $history = document.getElementById('js-history');
+
+const db = getFirestore(app);
+console.log(db)
+// Get a list of reports from your database
+const fetchReports = async () => {
+  //Get Collection 
+  const reportSnapshot = await getDocs(collection(db, 'reports'));
+  console.log(reportSnapshot)
+  let tags = '';
+  reportSnapshot.forEach(doc => {
+    console.log('id=>', doc.id)
+    tags += `<tr>
+      <td>${doc.data().date}</td>
+      <td>${doc.data().name}</td>
+      <td>${doc.data().work}</td>
+      <td>${doc.data().comment}</td>
+      </tr>`
+  });
+  $history.innerHTML = tags;
+}
+
+if($history){
+  fetchReports();
+}
