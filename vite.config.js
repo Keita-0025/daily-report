fb288eb6a7f51d550ca9daf2dc862ee55ef1471a
Keita-0025 <ku74n1@gmail.com>
2025-02-19 15:19:58 +0900

    chore: Firebase StoreへのデプロイとFirebase Hosting公開準備のため、設定ファイルを追加・修正

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+// import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    watch: {
+      include: 'src/**', // 監視するファイル
+      exclude: 'node_modules/**', // 除外するファイル
+    },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'history.html'),
+      },
+    },
+  },
 })
