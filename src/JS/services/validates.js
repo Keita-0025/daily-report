5941624cb8edff6e5aeb50bc0dd1a382a3cc2e59
Keita-0025 <ku74n1@gmail.com>
2025-02-27 13:00:22 +0900

    fix: バリデーションの値を変更

--- src/JS/services/validates.js
+++ src/JS/services/validates.js
@@ -3,13 +3,13 @@ const validateFormData = ({ name, work, comment }) => {
     if (!name || !work || !comment) {
       errors.push('すべてのフィールドに入力してください');
     }
-    if (name.length > 3) {
+    if (name.length > 50) {
       errors.push('名前は50文字以内で入力してください');
     }
-    if (work.length > 2) {
+    if (work.length > 200) {
       errors.push('作業内容は200文字以内で入力してください');
     }
-    if (comment.length > 2) {
+    if (comment.length > 200) {
       errors.push('コメントは200文字以内で入力してください');
     }
     return errors.length > 0 ? errors : null;
